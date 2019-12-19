4c0965cca700c7a8d17a10255d738b4f7a325816
JavierLete <javier@eskura21.com>
2019-12-19 10:50:19 +0100

    Validaciones

--- UF2216/src/com/ipartek/formacion/uf2216/presentacion/PresentacionConsola.java
+++ UF2216/src/com/ipartek/formacion/uf2216/presentacion/PresentacionConsola.java
@@ -12,28 +12,80 @@ public class PresentacionConsola {
 	public static void main(String[] args) {
 		Scanner scanner;
 		long ultimoId = 1L;
-		boolean repetir;
+		boolean esNuevoRegistro;
+		boolean esTituloCorrecto, esIsbnCorrecto, esNumeroPaginasCorrecto, esFormatoCorrecto;
+		String titulo, isbn;
+		int numeroPaginas = -1;
+		boolean formato = false;
+		String stringFormato;
+		
+		//Scanner sobre la entrada estándar
+		scanner = new Scanner(System.in);
 		
 		do {
-			//Scanner sobre la entrada estándar
-			scanner = new Scanner(System.in);
 			
-			//Introducción de título
-			System.out.print("Título: ");
-			String titulo = scanner.nextLine();
+			do {
+				//Introducción de título
+				System.out.print("Título: ");
+				titulo = scanner.nextLine();
+				
+				//Validación del título
+				if (titulo.length() >= 3 && titulo.length() <= 150) {
+					esTituloCorrecto = true;
+				} else {
+					esTituloCorrecto = false;
+					System.out.println("El título debe tener un tamaño mínimo de 3 letras y máximo de 150");
+				}
+			} while (!esTituloCorrecto);
 			
-			//Introducción de ISBN
-			System.out.print("ISBN: ");
-			String isbn = scanner.nextLine();
+			do {
+				//Introducción de ISBN
+				System.out.print("ISBN: ");
+				isbn = scanner.nextLine();
+				
+				if(isbn.matches("\\d{10}")) {
+					esIsbnCorrecto = true;
+				} else {
+					esIsbnCorrecto = false;
+					System.out.println("El ISBN debe ser un número de longitud 10");
+				}
+			} while (!esIsbnCorrecto);
 			
-			//Introducción de número de páginas
-			System.out.print("Número de páginas: ");
-			int numeroPaginas = scanner.nextInt();
-			scanner.nextLine();
+			do {
+				//Introducción de número de páginas
+				System.out.print("Número de páginas: ");
+				
+				try {
+					numeroPaginas = scanner.nextInt();
+					
+					if(numeroPaginas >= 1) {
+						esNumeroPaginasCorrecto = true;
+					} else {
+						esNumeroPaginasCorrecto = false;
+						System.out.println("El número de páginas debe ser mínimo 1");
+					}
+				} catch (Exception e) {
+					System.out.println("Debes introducir un número (sólo dígitos)");
+					esNumeroPaginasCorrecto = false;
+				} finally {
+					//Consumir el enter si ha ido bien, o el texto si ha habido una excepción
+					scanner.nextLine();
+				}
+			} while (!esNumeroPaginasCorrecto);
 			
-			//Introducción de formato
-			System.out.print("Formato (digital o papel): ");
-			boolean formato = scanner.nextLine().equals("digital");
+			do {
+				//Introducción de formato
+				System.out.print("Formato (digital o papel): ");
+				stringFormato = scanner.nextLine();
+				
+				if(stringFormato.equalsIgnoreCase("digital") || stringFormato.equalsIgnoreCase("papel")) {
+					esFormatoCorrecto = true;
+					formato = stringFormato.equalsIgnoreCase("digital");
+				} else {
+					esFormatoCorrecto = false;
+					System.out.println("El formato debe ser digital o papel");
+				}
+			} while (!esFormatoCorrecto);
 			
 			//Creación de libro en base a los datos introducidos
 			Libro libro = new Libro(ultimoId, titulo, isbn, numeroPaginas, formato);
@@ -49,8 +101,8 @@ public class PresentacionConsola {
 			
 			//Preguntar si el usuario quiere introducir un nuevo registro
 			System.out.print("¿Desea introducir un nuevo registro? (S/n) ");
-			repetir = !scanner.nextLine().equalsIgnoreCase("n");
-		} while (repetir);
+			esNuevoRegistro = !scanner.nextLine().equalsIgnoreCase("n");
+		} while (esNuevoRegistro);
 		
 		System.out.println("Saliendo de la aplicación. Gracias por usar esta aplicación");
 		
