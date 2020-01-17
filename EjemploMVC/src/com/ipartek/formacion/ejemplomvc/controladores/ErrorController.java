e33a66a0630c140953bbd1e526b4296ec19c5830
JavierLete <javier@eskura21.com>
2020-01-17 12:36:03 +0100

    Quitados comentarios

--- EjemploMVC/src/com/ipartek/formacion/ejemplomvc/controladores/ErrorController.java
+++ EjemploMVC/src/com/ipartek/formacion/ejemplomvc/controladores/ErrorController.java
@@ -15,11 +15,7 @@ public class ErrorController extends HttpServlet {
 		request.getRequestDispatcher("/WEB-INF/vistas/error.jsp").forward(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
