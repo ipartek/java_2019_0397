d6ea213b1a73abf39e2293acdfd97ae21296f58f
JavierLete <javier@eskura21.com>
2020-02-28 10:13:20 +0100

    Extracción de id con extraerId

--- EjemploRESTServlet/src/com/ipartek/formacion/ejemplorestservlet/UsuariosApi.java
+++ EjemploRESTServlet/src/com/ipartek/formacion/ejemplorestservlet/UsuariosApi.java
@@ -25,107 +25,138 @@ public class UsuariosApi extends HttpServlet {
 		usuarios.put(1L, new Usuario(1L, "javier@email.net", "javier"));
 		usuarios.put(2L, new Usuario(2L, "pepe@email.net", "pepe"));
 	}
-	
+
 	private static Gson gson = new Gson();
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		PrintWriter out = response.getWriter();
 
-		String path = request.getPathInfo();
+		Long id = null;
 
-		if (path == null || path.equals("/")) {
+		try {
+			id = extraerId(request);
+		} catch (Exception e) {
+			response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+			return;
+		}
+
+		if (id == null) {
 			out.write(gson.toJson(usuarios.values()));
 			// response.getWriter().write(new Gson().toJson(usuarios.values()));
-			
 			return;
 		}
 
-		if (path.matches(URL_ID_VALIDA)) {
-			Long id = Long.parseLong(path.substring(1));
+		Usuario usuario = usuarios.get(id);
 
-			Usuario usuario = usuarios.get(id);
-			
-			if(usuario == null) {
-				response.setStatus(HttpServletResponse.SC_NOT_FOUND);
-			} else {
-				out.write(gson.toJson(usuario));
-			}
-
-			return;
+		if (usuario == null) {
+			response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+		} else {
+			out.write(gson.toJson(usuario));
 		}
-
-		response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
 	}
 
-	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		try {
+			if (extraerId(request) != null) {
+				throw new Exception();
+			}
+		} catch (Exception e) {
+			response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+			return;
+		}
+
 		String json = extraerJSON(request);
-		
+
 		Usuario usuario = gson.fromJson(json, Usuario.class);
-		
+
 		Long id = usuarios.size() == 0 ? 1 : usuarios.lastKey() + 1;
 		usuario.setId(id);
-		
+
 		usuarios.put(id, usuario);
-		
+
 		response.getWriter().write(gson.toJson(usuario));
-		
+
 		response.setStatus(HttpServletResponse.SC_CREATED);
 	}
 
-
-	private String extraerJSON(HttpServletRequest request) throws IOException {
-		BufferedReader br = request.getReader();
-		
-		StringBuffer sb = new StringBuffer();
-		String linea;
-		
-		while( ( linea = br.readLine() ) != null ) {
-			sb.append(linea);
-		}
-		
-		return sb.toString();
-	}
-
 	@Override
-	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String path = request.getPathInfo();
+	protected void doPut(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		String json = extraerJSON(request);
-		
+
 		Usuario usuario = gson.fromJson(json, Usuario.class);
-		
-		if(path == null || !path.matches(URL_ID_VALIDA)) {
+
+		Long id = null;
+
+		try {
+			id = extraerId(request);
+
+			if (id == null) {
+				throw new Exception();
+			}
+		} catch (Exception e) {
 			response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
 			return;
 		}
-		
-		Long id = Long.parseLong(path.substring(1));
-		
-		if(id != usuario.getId()) {
+
+		if (id != usuario.getId()) {
 			response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
 			return;
 		}
-		
+
 		usuarios.put(id, usuario);
-		
+
 		response.getWriter().write(gson.toJson(usuario));
 	}
 
 	@Override
-	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String path = request.getPathInfo();
-		
-		if(path == null || !path.matches(URL_ID_VALIDA)) {
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		Long id = null;
+
+		try {
+			id = extraerId(request);
+
+			if (id == null) {
+				throw new Exception();
+			}
+		} catch (Exception e) {
 			response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
 			return;
 		}
-		
-		Long id = Long.parseLong(path.substring(1));
-		
+
 		usuarios.remove(id);
-		
+
 		response.getWriter().write("{}");
+	}
+
+	private static String extraerJSON(HttpServletRequest request) throws IOException {
+		BufferedReader br = request.getReader();
+
+		StringBuffer sb = new StringBuffer();
+		String linea;
+
+		while ((linea = br.readLine()) != null) {
+			sb.append(linea);
+		}
+
+		return sb.toString();
+	}
+
+	private static Long extraerId(HttpServletRequest request) {
+		String path = request.getPathInfo();
+
+		if (path == null || path.equals("/")) {
+			return null;
+		}
+
+		if (!path.matches(URL_ID_VALIDA)) {
+			throw new RuntimeException("URL de petición no válida");
+		}
+
+		return Long.parseLong(path.substring(1));
 	}
 }
