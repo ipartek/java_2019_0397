ca501bd5c0cd6690874d5e051a33f434cc716f32
JavierLete <javier@eskura21.com>
2020-03-03 13:33:57 +0100

    Ejemplo Hibernate con JPA contra MySQL

--- ejemplohibernate/src/main/java/com/ipartek/formacion/ejemplohibernate/App.java
+++ ejemplohibernate/src/main/java/com/ipartek/formacion/ejemplohibernate/App.java
@@ -1,13 +1,35 @@
 package com.ipartek.formacion.ejemplohibernate;
 
-/**
- * Hello world!
- *
- */
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	EntityManagerFactory entityManagerFactory = 
+    			Persistence.createEntityManagerFactory( "com.ipartek.formacion.ejemplohibernate" );
+    	
+    	EntityManager em = entityManagerFactory.createEntityManager();
+
+    	em.getTransaction().begin();
+    	
+    	em.persist(new Usuario("javier@email.net", "contra"));
+    	em.persist(new Usuario("pepe@email.net", "perez"));
+    	
+    	em.getTransaction().commit();
+    	
+    	em.getTransaction().begin();
+    	
+    	List<Usuario> usuarios = em.createQuery("from Usuario", Usuario.class).getResultList();
+    	
+    	for(Usuario usuario: usuarios) {
+    		System.out.println(usuario);
+    	}
+    	
+    	em.getTransaction().commit();
     }
 }
